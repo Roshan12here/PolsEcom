f53c887e4c66a9c47b77790556b4d61471d72081
Roshan Shamas <146016164+Roshan12here@users.noreply.github.com>
2024-11-08 23:24:11 +0500

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -25,9 +25,10 @@ export default async function DashboardLayout({
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  if (!user || user.email !== "roshangamercs2tf3@gmail.com") {
+if (!user || (user.email !== "roshangamercs2tf3@gmail.com" && user.email !== "nazariitomei50@gmail.com")) {
     return redirect("/");
   }
+  
   return (
     <div className="flex w-full flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
